e3acd9721c9a36ee970bfce47575b14cb104f1de
ptotem-toshiba-laptop <ptotem-toshiba-laptop@ptotemtoshibalaptop-Satellite-C665.(none)>
2014-10-07 16:16:46 +0530

    Monsters and Portals done, seer pending

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,19 +1,35 @@
-var portals = new Array();
-var count = 0;
+
+var selPort = [];
+var selMons = [];
+var flag = 0;
+var positions = [];
+
+
+function initEverything()
+{
+   SetPortal();
+    SetMonsters();
+    //SetSeer();
+}
 
 function SetPortal()
 {
-    var fg = 0;
-    var pID  = 0;
     var mL = 0;
     var mR = 0;
 
-    fg = Math.floor(Math.random() * (4-1) + 1);
+    flag = Math.floor(Math.random() * (4-1) + 1);
 
-    document.getElementById("console1").innerHTML = fg;
+    selPort = shuffle(portals);
 
-    for(var i = 1; i<=fg; i++)
+    //console.log(portals);
+    //console.log(selPort);
+
+    document.getElementById("console1").innerHTML = flag;
+
+    for(var i = 0; i<flag; i++)
     {
+        //draw.push(selPort);
+
         mL = Math.floor(Math.random() * (4-1) + 1);
         mR = Math.floor(Math.random() * (4-1) + 1);
 
@@ -26,22 +42,87 @@ function SetPortal()
         var pos = mL.toString() + mR.toString();
         var targetDiv = document.getElementById(pos.toString());
 
-        pID = Math.floor(Math.random() * (6-1) + 1);
-        if(pID == 1)
-            targetDiv.innerHTML = "Red";
-        if(pID == 2)
-            targetDiv.innerHTML = "Green";
-        if(pID == 3)
-            targetDiv.innerHTML = "Blue";
-        if(pID == 4)
-            targetDiv.innerHTML = "Yellow";
-        if(pID == 5)
-            targetDiv.innerHTML = "Orange";
-
-        portals.push({id:i, ps:pos, puID:pID});
-        count++;
+        targetDiv.style.backgroundColor = selPort[i].backgroundColor;
+        positions.push({ps:pos, posFlags:1});
     }
 
-    document.getElementById("console2").innerHTML = portals.puID;
+    //console.log(positions);
+}
 
+function SetSeer()
+{
+    var mL = 0;
+    var mR = 0;
+
+    mL = Math.floor(Math.random() * (4-1) + 1);
+    mR = Math.floor(Math.random() * (4-1) + 1);
+
+    while(mL == 2 && mR == 2)
+      {
+        mL = Math.floor(Math.random() * (4-1) + 1);
+        mR = Math.floor(Math.random() * (4-1) + 1);
+      }
+
+    var pos = mL.toString() + mR.toString();
+
+    for(var i=0; i<=positions.length; i++)
+    {
+        console.log(i);
+        if (positions[i].posFlags == 1)
+        {
+            mL = Math.floor(Math.random() * (4 - 1) + 1);
+            mR = Math.floor(Math.random() * (4 - 1) + 1);
+
+            while (mL == 2 && mR == 2)
+            {
+                mL = Math.floor(Math.random() * (4 - 1) + 1);
+                mR = Math.floor(Math.random() * (4 - 1) + 1);
+            }
+        }
+    }
+
+     pos = mL.toString() + mR.toString();
+
+    var targetDiv = document.getElementById(pos.toString());
+    console.log(targetDiv);
+
+    targetDiv.innerHTML = "<span style='color:#fff;'>SEER!</span>";
+    positions.push({ps:pos, posFlags:1});
+
+    //console.log(positions);
+}
+
+function SetMonsters()
+{
+    var mL = 0;
+    var mR = 0;
+
+    flag = Math.floor(Math.random() * (4-1) + 1);
+
+    selMons = shuffle(monsters);
+
+    //console.log(portals);
+    console.log(selMons);
+
+    for(var i = 1; i<=3; i++)
+    {
+        //draw.push(selPort);
+
+        mL = Math.floor(Math.random() * (4-1) + 1);
+        mR = Math.floor(Math.random() * (4-1) + 1);
+
+        while(mL == 2 && mR == 2)
+        {
+            mL = Math.floor(Math.random() * (4-1) + 1);
+            mR = Math.floor(Math.random() * (4-1) + 1);
+        }
+
+        var pos = mL.toString() + mR.toString();
+        var targetDiv = document.getElementById(pos.toString());
+
+        targetDiv.innerHTML = selMons[i].image;
+        positions.push({ps:pos, posFlags:1});
+    }
+
+    //console.log(positions);
 }
