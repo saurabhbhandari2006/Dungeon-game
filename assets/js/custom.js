89c0bc16ec6ad4e6e09c9da88380def06a2c677f
samrith-s <samrith.298@gmail.com>
2014-10-10 10:59:06 +0530

    Updated game files after discussion

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,236 +1,405 @@
 
-var selPort = [];       //Selected Portals
-var selMons = [];       //Selected Monsters
-var flag = 0;           //Flag value
-var portalPos = [];     //Positions of portals
-var monsterPos = [];    //Positions of monsters
-var posArr = [];        //Array of filled positions
-var mapID = 0;          //map ID
-var mapColor = 0;       //map color
-var c = 0;
+var mapHash;
+var entityHash = [];
 
-/* Function: initEverything
-    Description:
-        Main initialization function, which is called first in the program flow.
-    Status:
-        COMPLETED
- */
-function initEverything()
-{
-   enterDungeon();
- //   setMonsters();
-//     SetSeer();
+$(function() {
+    createMap();
+    createEntity();
+    addEntities("Purple");
+
+});
+
+function initializeTheme() {
+
 }
 
-/*
-    Function: getPosition
-    Description:
-        Position getter function. Calculates positions after checking for conditions.
-    Status:
-        COMPLETED
- */
-function getPosition(flag)
-{
-    c = 0;
-    var pos;
-    posArr = [];
+function createMap() {
 
-    while (c < flag) {
+    mapHash = map;
 
-        pos = getRandom();
-        if(c == 0) {
-            posArr.push(pos);
-            c++;
-        } else {
-            if(validatePos(pos)) {
-                posArr.push(pos);
-                c++;
+    for(var i=0; i<mapHash.length; i++)
+    {
+        var imgName = mapHash[i].name;
+        mapHash[i].id = i+1;
+        mapHash[i].backgroundImage = "assets/img/backgrounds/" + imgName + ".png";
+    }
+
+    //console.log(mapHash);
+}
+
+function createEntity() {
+
+    for(var i=0; i < monsters.length; i++)
+    {
+       entityHash.push(monsters[i]);
+       entityHash[i].id = i+1;
+    }
+
+    for(var i = 0; i < mapHash.length; i++) {
+        var imgName = mapHash[i].name;
+
+        entityHash.push(
+            {
+                id:entityHash.length,
+                portal:imgName,
+                portalImage:"assets/img/portals/" + imgName + ".png"
             }
-
-        }
-
+        );
     }
 
+    console.log(entityHash);
 }
 
-function getRandom() {
-    var mL = 0;
-    var mR = 0;
+function addEntities(dungeon) {
 
-    mL = Math.floor(Math.random() * (4-1) + 1);
-    mR = Math.floor(Math.random() * (4-1) + 1);
+    var x = 0;
+    var y = 0;
 
-    while(mL == 2 && mR == 2)
+    x = Math.floor(Math.random() * 3 + 1);
+    y = Math.floor(Math.random() * 3 + 1);
+
+    if(dungeon=="Purple")
     {
-        mL = Math.floor(Math.random() * (4-1) + 1);
-        mR = Math.floor(Math.random() * (4-1) + 1);
-    }
+        var dungeonSelect = $.grep(mapHash, function(element) {
+            return element.id == 1;
+        });
 
-    var pos = mL.toString() + mR.toString();
-    return pos;
-}
+        dungeonSelect.lx = x;
+        dungeonSelect.ly = y;
 
-function validatePos(pos) {
-    var can = true;
-    for (var i = 0; i < posArr.length; i++) {
-        if (pos == posArr[i])
-            can = false;
-    }
-
-    return can;
-
-}
-
-function enterDungeon()
-{
-    mapID++;
-
-    selPort = shuffle(portals);
-    selMons = shuffle(monsters);
-
-    if(mapID==1)
-    {
-        document.getElementById("mainmatrix").style.backgroundColor = selPort[0].backgroundColor;
-        mapColor=selPort[0].colorID;
-            setPortal();
-    }
-    else
-    {
-        mapColor=portals[x].colorID;
-            setPortal();
-    }
-
-    setMonsters();
-    setPlayer();
-}
-
-
-function setPlayer()
-{
-    var pos = "22";
-
-    var targetDiv = document.getElementById(pos);
-
-    targetDiv.innerHTML = "<img src='assets/img/player.png' />";
-}
-/*
-    Function: SetPortal
-    Description:
-        Function to randomly select and place 3 portals on the map.
-    Status:
-        COMPLETED
- */
-function setPortal()
-{
-    var pos;
-
-    flag = Math.floor(Math.random() * 3 + 1);
-    console.log(flag);
-    getPosition(flag);
-
-
-
-    for(var i=0; i<flag; i++)
-    {
-        console.log(i);
-        var targetDiv = document.getElementById(posArr[i]);
-
-        if(selPort[i].colorID != mapColor)
+        if(dungeonSelect.entity!=0)
         {
-            targetDiv.style.backgroundColor = selPort[i].backgroundColor;
-            console.log(selPort[i]);
+            x = Math.floor(Math.random() * 3 + 1);
+            y = Math.floor(Math.random() * 3 + 1);
         }
         else
         {
-            targetDiv.style.backgroundColor = selPort[i+3].backgroundColor;
-            console.log(selPort);
+            var monsterAdd = $.grep(entityHash, )
         }
 
-        console.log(targetDiv);
-        portalPos.push({mapID:mapID, colID:selPort[i].colorID, getPos:pos});
+        console.log(dungeonSelect);
     }
-
-    console.log(portalPos);
 }
 
-/*
-    Function: SetMonsters
-    Description:
-        Function to randomly assign 3 monsters on the map.
-    Status:
-        COMPLETED
- */
-function setMonsters()
-{
 
 
-    flag = Math.floor(Math.random() * 3 +1);
-    console.log(flag);
-    getPosition(flag);
 
-    for(var i = 0; i<flag; i++)
-    {
-        var targetDiv = document.getElementById(posArr[i]);
 
-        targetDiv.innerHTML = selMons[i].image;
 
-        console.log(targetDiv);
 
-        //monsterPos.push()
-    }
 
-    console.log(posArr);
-}
 
-function drawMonsters()
-{
 
-}
 
-/*
-    Function: SetSeer
-    Description:
-        Function to randomly assign either 1 or no Seers on the map grid
-    Status:
-        Work in progress
- */
-function SetSeer()
-{
-    var mL = 0;
-    var mR = 0;
 
-    mL = Math.floor(Math.random() * 3 + 1);
-    mR = Math.floor(Math.random() * 3 + 1);
 
-    while(mL == 2 && mR == 2)
-      {
-        mL = Math.floor(Math.random() * 3 + 1);
-        mR = Math.floor(Math.random() * 3 + 1);
-      }
 
-    var pos = mL.toString() + mR.toString();
 
-    for(var i=0; i<=positions.length; i++)
-    {
-        console.log(i);
-        if (portalPos[i].ps == pos)
-        {
-            mL = Math.floor(Math.random() * 3 + 1);
-            mR = Math.floor(Math.random() * 3 + 1);
 
-            while (mL == 2 && mR == 2)
-            {
-                mL = Math.floor(Math.random() * 3 + 1);
-                mR = Math.floor(Math.random() * 3 + 1);
-            }
-        }
-    }
 
-     pos = mL.toString() + mR.toString();
 
-    var targetDiv = document.getElementById(pos.toString());
-    console.log(targetDiv);
 
-    targetDiv.innerHTML = "<span style='color:#fff;'>SEER!</span>";
-    positions.push({ps:pos, posFlags:1});
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//************************************************************************************************************************
+//var selPort = [];       //Selected Portals
+//var selMons = [];       //Selected Monsters
+//var flag = 0;           //Flag value
+//var portalPos = [];     //Positions of portals
+//var monsterPos = [];    //Positions of monsters
+//var posArr = [];        //Array of filled positions
+//var mapID = 0;          //map ID
+//var mapColor = 0;       //map color
+//var element = 1;
+//var monsterList = monsters;
+//
+///* Function: initEverything
+//    Description:
+//        Main initialization function, which is called first in the program flow.
+//    Status:
+//        COMPLETED
+// */
+//function initEverything()
+//{
+//   enterDungeon();
+// //   setMonsters();
+////     SetSeer();
+//}
+//
+///*
+//    Function: getPosition
+//    Description:
+//        Position getter function. Calculates positions after checking for conditions.
+//    Status:
+//        COMPLETED
+// */
+//function getPosition(flag)
+//{
+//    c = 0;
+//    var pos;
+//    posArr = [];
+//
+//    switch(element)
+//    {
+//        case 1:
+//                while (c < flag)
+//                {
+//                    pos = getRandom();
+//                    if(c == 0)
+//                    {
+//                        posArr.push(pos);
+//                        c++;
+//                    }
+//                    else
+//                    {
+//                        if(validatePos(pos))
+//                        {
+//                            posArr.push(pos);
+//                            c++;
+//                        }
+//                    }
+//                }
+//                break;
+//
+//        case 2:
+//                pos = getRandom();
+//                if(validatePos(pos))
+//                {
+//                    posArr.push(pos);
+//                    c++;
+//                }
+//                break;
+//    }
+//
+//}
+//
+//function getRandom() {
+//    var mL = 0;
+//    var mR = 0;
+//
+//    mL = Math.floor(Math.random() * (4-1) + 1);
+//    mR = Math.floor(Math.random() * (4-1) + 1);
+//
+//    while(mL == 2 && mR == 2)
+//    {
+//        mL = Math.floor(Math.random() * (4-1) + 1);
+//        mR = Math.floor(Math.random() * (4-1) + 1);
+//    }
+//
+//    var pos = mL.toString() + mR.toString();
+//    return pos;
+//}
+//
+//function validatePos(pos) {
+//    var can = true;
+//    for (var i = 0; i < posArr.length; i++) {
+//        if (pos == posArr[i])
+//            can = false;
+//    }
+//
+//    return can;
+//
+//}
+//
+//function enterDungeon()
+//{
+//    selPort = shuffle(portals);
+//    selMons = shuffle(monsters);
+//
+//    if(mapColor==0)
+//    {
+//        document.getElementById("mainmatrix").style.backgroundColor = selPort[mapColor].backgroundColor;
+//        mapColor=selPort[0].colorID;
+//            setPortal();
+//    }
+//    else
+//    {
+//        mapColor=selPort[x].colorID;
+//            setPortal();
+//    }
+//
+//    setMonsters();
+//    setPlayer();
+//    setSeer();
+//}
+//
+//
+//function setPlayer()
+//{
+//    var pos = "22";
+//
+//    var targetDiv = document.getElementById(pos);
+//
+//    targetDiv.innerHTML = "<img src='assets/img/player.png' />";
+//}
+///*
+//    Function: SetPortal
+//    Description:
+//        Function to randomly select and place 3 portals on the map.
+//    Status:
+//        COMPLETED
+// */
+//function setPortal()
+//{
+//    var pos;
+//
+//    flag = Math.floor(Math.random() * 3 + 1);
+//    console.log(flag);
+//
+//    element = 1;
+//    getPosition(flag);
+//
+//
+//
+//    for(var i=0; i<flag; i++)
+//    {
+//        console.log(i);
+//        var targetDiv = document.getElementById(posArr[i]);
+//
+//        if(selPort[i].colorID != mapColor)
+//        {
+//            targetDiv.style.backgroundColor = selPort[i].backgroundColor;
+//            console.log(selPort[i]);
+//        }
+//        else
+//        {
+//            targetDiv.style.backgroundColor = selPort[i+3].backgroundColor;
+//            console.log(selPort);
+//        }
+//
+//        console.log(targetDiv);
+//        portalPos.push({colID:selPort[i].colorID, getPos:posArr[i]});
+//    }
+//
+//    console.log(portalPos);
+//}
+//
+///*
+//    Function: SetMonsters
+//    Description:
+//        Function to randomly assign 3 monsters on the map.
+//    Status:
+//        COMPLETED
+// */
+//function setMonsters()
+//{
+//    flag = Math.floor(Math.random() * 3 +1);
+//    console.log(flag);
+//
+//    element = 1;
+//    getPosition(flag);
+//
+//    for(var i = 0; i<flag; i++)
+//    {
+//        var targetDiv = document.getElementById(posArr[i]);
+//
+//        targetDiv.innerHTML = selMons[i].image;
+//
+//        console.log(targetDiv);
+//
+//        monsterPos.push({colID:selMons[i].id, getPos:posArr[i]})
+//        monsterList[i].health--;
+//        console.log("Monster health: " + monsterList[i].health);
+//    }
+//
+//    console.log(posArr);
+//}
+//
+///*
+//    Function: SetSeer
+//    Description:
+//        Function to randomly assign either 1 or no Seers on the map grid
+//    Status:
+//        Work in progress
+// */
+//function setSeer()
+//{
+////    var set = false;
+////    flag = Math.floor(Math.random() * 1 + 1);
+////
+////    if(flag == 0)
+////        set = true;
+////
+////    element = 2;
+////
+////    while (set == false) {
+////        getPosition(flag);
+////        for (var b = 0, c = 0; b < monsterPos.length, c < portalPos.length; b++, c++)
+////            if(posArr[i]!= monsterPos[c].getPos || posArr[i] != portalPos[b].getPos){
+////                var targetDiv = document.getElementById(posArr[i]);
+////                targetDiv.innerHTML = "SEER!";
+////                set = true;
+////            } else {getPosition(flag);}
+////    }
+//
+//
+//
+//    var c = 0;
+//    var b = 0;
+//    var i = 0;
+//
+//    flag = 1;//Math.floor(Math.random() * 1 + 1);
+//
+//    element = 2;
+//    if(flag!=0)
+//    {
+//        getPosition(flag);
+//        for(b=0; b<monsterPos.length; b++)
+//        {
+//            while(posArr[i]==monsterPos[b].getPos)
+//            {
+//                getPosition(flag);
+//            }
+//            while(posArr[i] == portalPos[c].getPos)
+//            {
+//                getPosition(flag);
+//            }
+//
+//            var targetDiv = document.getElementById(posArr[i]);
+//            targetDiv.innerHTML = "SEER!"
+//
+//            console.log("Seer position: " + posArr[i]);
+//            console.log("Seer flag: " + flag);
+//            c++;
+//            b++;
+//        }
+//    }
+//}
--- assets/js/dungeon-data.js
+++ assets/js/dungeon-data.js
@@ -1,39 +1,238 @@
+var initHealth = 100;
+var initDice = 2;
+var teleportCost = 5;
 
 /*
-    Data: Monsters
-    Description:
-        Holds the data for the various kinds of monsters, to be randomly outputted.
+
+ */
+var map = [
+
+    {name: "Purple", type: "definition", generation: {},
+        monsters:3,
+        portals:2,
+        seer: 0,
+        definition: [
+            {lx: 1, ly: 1, entity: 0},
+            {lx: 1, ly: 2, entity: 0},
+            {lx: 1, ly: 3, entity: 0},
+            {lx: 2, ly: 1, entity: 0},
+            {lx: 2, ly: 2, entity: 0},
+            {lx: 2, ly: 3, entity: 0},
+            {lx: 3, ly: 1, entity: 0},
+            {lx: 3, ly: 2, entity: 0},
+            {lx: 3, ly: 3, entity: 0}
+        ]
+    },
+
+    {name: "Blue",  type: "definition", generation: {},
+        monsters:3,
+        portals:2,
+        seer: 0,
+        definition: [
+            {lx: 1, ly: 1, entity: 0},
+            {lx: 1, ly: 2, entity: 0},
+            {lx: 1, ly: 3, entity: 0},
+            {lx: 2, ly: 1, entity: 0},
+            {lx: 2, ly: 2, entity: 0},
+            {lx: 2, ly: 3, entity: 0},
+            {lx: 3, ly: 1, entity: 0},
+            {lx: 3, ly: 2, entity: 0},
+            {lx: 3, ly: 3, entity: 0}
+        ]
+    },
+
+    {name: "Green",  type: "definition", generation: {},
+        monsters:3,
+        portals:2,
+        seer: 1,
+        definition: [
+            {lx: 1, ly: 1, entity: 0},
+            {lx: 1, ly: 2, entity: 0},
+            {lx: 1, ly: 3, entity: 0},
+            {lx: 2, ly: 1, entity: 0},
+            {lx: 2, ly: 2, entity: 0},
+            {lx: 2, ly: 3, entity: 0},
+            {lx: 3, ly: 1, entity: 0},
+            {lx: 3, ly: 2, entity: 0},
+            {lx: 3, ly: 3, entity: 0}
+        ]
+    },
+
+    {name: "Yellow",  type: "definition", generation: {},
+        monsters:3,
+        portals:2,
+        seer: 1,
+        definition: [
+            {lx: 1, ly: 1, entity: 0},
+            {lx: 1, ly: 2, entity: 0},
+            {lx: 1, ly: 3, entity: 0},
+            {lx: 2, ly: 1, entity: 0},
+            {lx: 2, ly: 2, entity: 0},
+            {lx: 2, ly: 3, entity: 0},
+            {lx: 3, ly: 1, entity: 0},
+            {lx: 3, ly: 2, entity: 0},
+            {lx: 3, ly: 3, entity: 0}
+        ]
+    },
+
+    {name: "Orange",  type: "definition", generation: {},
+        monsters:3,
+        portals:2,
+        seer: 1,
+        definition: [
+            {lx: 1, ly: 1, entity: 0},
+            {lx: 1, ly: 2, entity: 0},
+            {lx: 1, ly: 3, entity: 0},
+            {lx: 2, ly: 1, entity: 0},
+            {lx: 2, ly: 2, entity: 0},
+            {lx: 2, ly: 3, entity: 0},
+            {lx: 3, ly: 1, entity: 0},
+            {lx: 3, ly: 2, entity: 0},
+            {lx: 3, ly: 3, entity: 0}
+        ]
+    },
+
+    {name: "Red",  type: "definition", generation: {},
+        monsters:3,
+        portals:2,
+        seer: 1,
+        definition: [
+            {lx: 1, ly: 1, entity: 0},
+            {lx: 1, ly: 2, entity: 0},
+            {lx: 1, ly: 3, entity: 0},
+            {lx: 2, ly: 1, entity: 0},
+            {lx: 2, ly: 2, entity: 0},
+            {lx: 2, ly: 3, entity: 0},
+            {lx: 3, ly: 1, entity: 0},
+            {lx: 3, ly: 2, entity: 0},
+            {lx: 3, ly: 3, entity: 0}
+        ]
+    }
+]
+/*
+ Data: Monsters
+ Description:
+ Holds the data for the various kinds of monsters, to be randomly outputted.
  */
 var monsters = [
 
-    {name: "The Amphisbaena", category: "monster", health: "30", dice:"1",image: "<img src='assets/img/m1.png' />"},
-    {name: "The Caristae", category: "monster", health: "40", dice:"2",image: "<img src='assets/img/m2.png' />"},
-    {name: "The Echeneis", category: "monster", health: "50", dice:"3",image: "<img src='assets/img/m3.png' />"},
-    {name: "The Ghul ", category: "monster", health: "60", dice:"4",image: "<img src='assets/img/m4.png' />"},
-    {name: "The Aniluus", category: "monster", health: "70", dice:"5",image: "<img src='assets/img/m5.png' />"},
-    {name: "The Ogre", category: "monster", health: "80", dice:"6",image: "<img src='assets/img/m6.png' />"}
-    ]
+    {
+        name: "Zortran",
+        type: "Wraith",
+        health: 20,
+        dice: 1,
+        probability:0,
+        level: 1,
+        power:"",
+        image: "<img src='assets/img/m1.png' />"
+    },
+    {
+        name: "Phinos",
+        type: "Necromancer",
+        health: 20,
+        dice: 1,
+        probability:0,
+        level: 1,
+        image: "<img src='assets/img/m1.png' />"
+    },
+    {
+        name: "Magina",
+        type: "Mage",
+        health: 30,
+        dice: 2,
+        probability:0,
+        level: 2,
+        power:"",
+        image: "<img src='assets/img/m2.png' />"
+    },
+    {
+        name: "Karo",
+        type: "Golem",
+        health: 30,
+        dice: 2,
+        probability:0,
+        level: 2,
+        power:"",
+        image: "<img src='assets/img/m2.png' />"
+    },
+    {
+        name: "Echenist",
+        type: "Warlock",
+        health: 40,
+        dice: 2,
+        probability:0,
+        level: 3,
+        power:"",
+        image: "<img src='assets/img/m3.png' />"
+    },
+    {
+        name: "Krobelus",
+        type: "Prophet",
+        health: 40,
+        dice: 2,
+        probability:0,
+        level: 3,
+        power:"",
+        image: "<img src='assets/img/m3.png' />"
+    },
+    {
+        name: "Ghul",
+        type: "Undead",
+        health: 50,
+        dice: 3,
+        probability:0,
+        level: 4,
+        power:"",
+        image: "<img src='assets/img/m4.png' />"
+    },
+    {
+        name: "Raxtax",
+        type: "Spirit" ,
+        health: 50,
+        dice: 4,
+        probability:0,
+        level: 4,
+        power:"",
+        image: "<img src='assets/img/m4.png' />"
+    },
+    {
+        name: "Enormo",
+        type: "Giant" ,
+        health: 60,
+        dice: 4,
+        probability:0,
+        level: 5,
+        power:"",
+        image: "<img src='assets/img/m4.png' />"
+    },
+    {
+        name: "Ghaijin",
+        type: "Troll" ,
+        health: 60,
+        dice: 4,
+        probability:0,
+        level: 5,
+        power:"",
+        image: "<img src='assets/img/m4.png' />"
+    },
+    {
+        name: "Infernus",
+        type: "Dragon",
+        health: 100,
+        dice: 5,
+        probability: 0,
+        level: 10,
+        power:"",
+        image:""
+    }
+]
 
 /*
-    Data: Portals
-    Description:
-        Holds the data for the various colours of portals, to be randomly outputted.
+ Function: shuffle
+ Description:
+ Shuffles data to maximise effective randomization.
  */
-var portals = [
-
-    {name: "Red", colorID: "1", backgroundColor:"red"},
-    {name: "Green", colorID: "2", backgroundColor:"green"},
-    {name: "Blue", colorID: "3", backgroundColor:"blue"},
-    {name: "Yellow", colorID: "4", backgroundColor:"yellow"},
-    {name: "Orange", colorID: "5", backgroundColor:"orange"}
-    ]
-
-/*
-    Function: shuffle
-    Description:
-        Shuffles data to maximise effective randomization.
- */
-function shuffle(o){
-    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+function shuffle(o) {
+    for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
     return o;
 }
