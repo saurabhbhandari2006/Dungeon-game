5e4b4736a3611beac157b9c4024492d6942aa949
agarwalmehul <agarwalmehul92@gmail.com>
2014-10-31 15:31:04 +0530

    4 questions added

--- assets/js/questionbank.js
+++ assets/js/questionbank.js
@@ -1,20 +1,24 @@
 window.quiz = {};
 quiz.questions = [
     [
-        {id: 1, difficulty: "Easy", name: "In which city is the annual Running of the Bulls festival held?", opta: "Pamplona", optb: "Madrid", optc: "Valencia", optd: "Bunol" }
+        {id: 1, difficulty: "Easy", name: "Any and all the gifts that are to be given to a Government Official must have received a prior written approval of...", opta: "The HOD of the department you are part of", optb: "The CGC", optc: "Both of the above", optd: "None of the above" }
     ],
     [
-        {id: 2, difficulty: "Normal", name: "The Guindy National Park is located within the urban limits of which state capital in India?", opta: "Mumbai", optb: "Hyderabad", optc: "Chennai", optd: "Cochin" }
+        {id: 2, difficulty: "Normal", name: "A Client gives you a watch worth Rs. 3,000/- for your wedding. What would you do?", opta: "Accept but get approval from the HOD of your department", optb: "Accept but get prior approval from the CGC", optc: "It's your wedding! You don't need permission to accept gifts.", optd: "None of the above." }
     ],
     [
-        {id: 3, difficulty: "Difficult", name: "In which country can you see the Catatumbo Lightning, a spectacular natural phenomenon?", opta: "India", optb: "Russia", optc: "Venezuela", optd: "USA" }
+        {id: 3, difficulty: "Difficult", name: "What should you do if you find out that your colleague has accepted a gift worth Rs. 10,000/- from one of Star TV's vendors?", opta: "Do nothing! It's not your business.", optb: "Speak to your colleague and ask for a share of the gift.", optc: "Report the case to the CGC", optd: "Speak to yor boss about it hoping that your colleague will get reprimanded" }
+    ],
+    [
+        {id: 4, difficulty: "Difficult", name: "A representative of a charitable organization has requested you for a donation of Rs. 1,000/- on behalf of Star India. Would you need approvals to do so?", opta: "Of course not! It's only a thousand rupee", optb: "Yes. Prior approval from the HOD and the CEO or CFO", optc: "Yes. Prior approval from the CFO only", optd: "No approvals are required for charity as long as they are less than Rs. 5,000/-" }
     ]
 ];
 
 quiz.answers = [
-    {id: 1, correct: "opta", payoff: 5, correct_answer: "Pamplona"},
-    {id: 2, correct: "optc", payoff: 10, correct_answer: "Chennai"},
-    {id: 3, correct: "optc", payoff: 20, correct_answer: "Venezuela"}
+    {id: 1, correct: "optc", payoff: 5, correct_answer: "Both of the above"},
+    {id: 2, correct: "opta", payoff: 10, correct_answer: "Accept but get approval from the HOD of your department"},
+    {id: 3, correct: "optc", payoff: 15, correct_answer: "Report the case to the CGC"},
+    {id: 3, correct: "optb", payoff: 20, correct_answer: "Yes. Prior approval from the HOD and the CEO or CFO"}
 ];
 
 function getAnswer(id, answer) {
